35336f42d41bed1a1cdc93b16add939e37dea6cb
Edgar Baluku <edgarbaluku@gmail.com>
2023-08-19 12:33:31 +0300

    funcations and lists

---
+++ Loops.py
@@ -0,0 +1,7 @@
+for i in range(5):
+    print(i)  # Prints numbers from 0 to 4
+
+num = 0
+while num < 5:
+    print(num)
+    num += 1 #program is incremented by 1 from 0 and less than 5
---
+++ funcantions.py
@@ -0,0 +1,4 @@
+def call(name):
+    print("my name is  " +name)
+
+call("Edgar")
