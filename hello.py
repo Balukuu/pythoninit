3a0615f91735be09588f1747b56a6d6914cb74ee
Edgar Baluku <edgarbaluku@gmail.com>
2023-09-22 21:29:50 +0300

    2 functions

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
+def main():
+    name= input("whats your name")
+    hello(name)
 
 def hello(to="world"):
     print("hello,", to)
-
-hello()
-name = input("what's your name?")    
-hello(name)
+main()
