e9d6e551349c3c30bab36afc5614fa8f260e9b73
Edgar Baluku <edgarbaluku@gmail.com>
2023-09-22 21:35:51 +0300

    simple square

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
-x=float(input("What's x ?"))
-y=float(input("what's y ?"))
-z=round(x /y, 2)
+def main():
+    x=int(input("What's x ?"))
+    print("x squared is", square(x))
 
-print(f"{z:,}")
+def square(n): 
+    return pow(n,2 ) 
+
+main()
