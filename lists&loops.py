903c35d539aeda7e10e2d0ed3cd083f7857e2afd
Edgar Baluku <edgarbaluku@gmail.com>
2023-11-09 20:17:03 +0300

    list

---
+++ lists&loops.py
@@ -0,0 +1,15 @@
+# Creating a list
+fruits = ["apple", "banana", "orange", "grape"]
+
+# Looping through the list
+print("Printing fruits using a for loop:")
+for fruit in fruits:
+    print(fruit)
+
+# Adding an element to the list
+fruits.append("watermelon")
+
+# Looping through the updated list
+print("\nPrinting updated fruits using a for loop:")
+for fruit in fruits:
+    print(fruit)
