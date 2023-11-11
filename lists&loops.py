cc03eefeeec3358a49f5535e133fe63a47886c2d
Edgar Baluku <edgarbaluku@gmail.com>
2023-11-11 10:09:10 +0300

    updated

--- lists&loops.py
+++ lists&loops.py
@@ -1,15 +1,19 @@
-# Creating a list
-fruits = ["apple", "banana", "orange", "grape"]
+# Creating a list of numbers
+numbers = [1, 2, 3, 4, 5]
 
-# Looping through the list
-print("Printing fruits using a for loop:")
-for fruit in fruits:
-    print(fruit)
+# Looping through the list and printing each element
+print("Printing elements of the list:")
+for number in numbers:
+    print(number)
 
-# Adding an element to the list
-fruits.append("watermelon")
+# Summing up the elements of the list using a loop
+sum_result = 0
+for number in numbers:
+    sum_result += number
+print("\nSum of elements in the list:", sum_result)
 
-# Looping through the updated list
-print("\nPrinting updated fruits using a for loop:")
-for fruit in fruits:
-    print(fruit)
+# Using a loop to create a new list with squared elements
+squared_numbers = []
+for number in numbers:
+    squared_numbers.append(number ** 2)
+print("\nSquared elements of the list:", squared_numbers)
